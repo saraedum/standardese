e78a7f385f65046a1c55508aec26544dea1aaec7
Julian Rüth <julian.rueth@fsfe.org>
2021-04-22 01:41:57 +0200

    make markdown generator more robust with invalid models

--- src/output_generator/markdown/markdown_generator.cpp
+++ src/output_generator/markdown/markdown_generator.cpp
@@ -5,6 +5,7 @@
 
 #include <cassert>
 #include <cmark-gfm.h>
+#include <fmt/format.h>
 
 #include "../../../standardese/output_generator/markdown/markdown_generator.hpp"
 #include "../../../standardese/model/markup/code_block.hpp"
@@ -14,25 +15,28 @@
 #include "../../../standardese/model/markup/text.hpp"
 #include "../../../standardese/model/markup/image.hpp"
 #include "../../../standardese/model/cpp_entity_documentation.hpp"
+#include "../../../standardese/logger.hpp"
 
 namespace standardese::output_generator::markdown
 {
 
 namespace {
 
-cmark_node* create_child(cmark_node* top, cmark_node_type type) {
+cmark_node* append_child(cmark_node* top, cmark_node_type type) {
   // TODO: Use the safe cmark wrapper from the extensions here.
   auto node = cmark_node_new(type);
   int success = cmark_node_append_child(top, node);
-  assert(success);
+  if (!success)
+    logger::error(fmt::format("Could not insert a MarkDown node of type `{}` into node of type `{}`. The node and its children will be missing from the output.", cmark_node_get_type_string(node), cmark_node_get_type_string(top)));
   return node;
 }
 
-cmark_node* insert_child(cmark_node* top, cmark_node_type type) {
+cmark_node* prepend_child(cmark_node* top, cmark_node_type type) {
   // TODO: Use the safe cmark wrapper from the extensions here.
   auto node = cmark_node_new(type);
   int success = cmark_node_prepend_child(top, node);
-  assert(success);
+  if (!success)
+    logger::error(fmt::format("Could not insert a MarkDown node of type `{}` into node of type `{}`. The node and its children will be missing from the output.", cmark_node_get_type_string(node), cmark_node_get_type_string(top)));
   return node;
 }
 
@@ -50,19 +54,19 @@ markdown_generator::~markdown_generator() {
 }
 
 void markdown_generator::visit(block_quote& block_quote) {
-    top = create_child(top, CMARK_NODE_BLOCK_QUOTE);
+    top = append_child(top, CMARK_NODE_BLOCK_QUOTE);
     stream_generator::visit(block_quote);
     top = cmark_node_parent(top);
 }
 
 void markdown_generator::visit(code& code) {
-    top = create_child(top, CMARK_NODE_CODE);
+    top = append_child(top, CMARK_NODE_CODE);
     stream_generator::visit(code);
     top = cmark_node_parent(top);
 }
 
 void markdown_generator::visit(code_block& code_block) {
-    top = create_child(top, CMARK_NODE_CODE_BLOCK);
+    top = append_child(top, CMARK_NODE_CODE_BLOCK);
 
     if (!code_block.language.empty())
         cmark_node_set_fence_info(top, code_block.language.c_str());
@@ -72,17 +76,17 @@ void markdown_generator::visit(code_block& code_block) {
 }
 
 void markdown_generator::visit(emphasis& emphasis) {
-    top = create_child(top, CMARK_NODE_EMPH);
+    top = append_child(top, CMARK_NODE_EMPH);
     stream_generator::visit(emphasis);
     top = cmark_node_parent(top);
 }
 
 void markdown_generator::visit(hard_break& hard_break) {
-    create_child(top, CMARK_NODE_LINEBREAK);
+    append_child(top, CMARK_NODE_LINEBREAK);
 }
 
 void markdown_generator::visit(heading& heading) {
-    top = create_child(top, CMARK_NODE_HEADING);
+    top = append_child(top, CMARK_NODE_HEADING);
 
     cmark_node_set_heading_level(top, heading.level);
 
@@ -91,7 +95,7 @@ void markdown_generator::visit(heading& heading) {
 }
 
 void markdown_generator::visit(link& link) {
-    top = create_child(top, CMARK_NODE_LINK);
+    top = append_child(top, CMARK_NODE_LINK);
 
     if (!link.title.empty())
       cmark_node_set_title(top, link.title.c_str());
@@ -104,8 +108,10 @@ void markdown_generator::visit(link& link) {
         cmark_node_set_url(top, ("sphinx://" + target.project + "@" + target.entry.uri).c_str());
       } else if (link.target.href().has_value()) {
         cmark_node_set_url(top, link.target.href().value().c_str());
+      } else if constexpr (std::is_same_v<T, model::link_target::cppast_target>) {
+        logger::error(fmt::format("Cannot render link to `{}` as MarkDown since it has not been resolved to a URL.", target.target->name()));
       } else {
-        throw std::logic_error("not implemented: cannot render this kind of link as MarkDown");
+        logger::error("Cannot render this kind of link as MarkDown yet.");
       }
     });
 
@@ -114,13 +120,13 @@ void markdown_generator::visit(link& link) {
 }
 
 void markdown_generator::visit(list_item& list_item) {
-    top = create_child(top, CMARK_NODE_ITEM);
+    top = append_child(top, CMARK_NODE_ITEM);
     stream_generator::visit(list_item);
     top = cmark_node_parent(top);
 }
 
 void markdown_generator::visit(list& list) {
-    top = create_child(top, CMARK_NODE_LIST);
+    top = append_child(top, CMARK_NODE_LIST);
 
     cmark_node_set_list_type(top, list.ordered ? CMARK_ORDERED_LIST : CMARK_BULLET_LIST);
 
@@ -129,17 +135,17 @@ void markdown_generator::visit(list& list) {
 }
 
 void markdown_generator::visit(paragraph& paragraph) {
-    top = create_child(top, CMARK_NODE_PARAGRAPH);
+    top = append_child(top, CMARK_NODE_PARAGRAPH);
     stream_generator::visit(paragraph);
     top = cmark_node_parent(top);
 }
 
 void markdown_generator::visit(soft_break& soft_break) {
-    create_child(top, CMARK_NODE_SOFTBREAK);
+    append_child(top, CMARK_NODE_SOFTBREAK);
 }
 
 void markdown_generator::visit(strong_emphasis& strong_emphasis) {
-    top = create_child(top, CMARK_NODE_STRONG);
+    top = append_child(top, CMARK_NODE_STRONG);
     stream_generator::visit(strong_emphasis);
     top = cmark_node_parent(top);
 }
@@ -154,13 +160,13 @@ void markdown_generator::visit(text& text) {
             cmark_node_set_literal(top, "");
         cmark_node_set_literal(top, (cmark_node_get_literal(top) + text.value).c_str());
     } else {
-      auto node = create_child(top, CMARK_NODE_TEXT);
+      auto node = append_child(top, CMARK_NODE_TEXT);
       cmark_node_set_literal(node, text.value.c_str());
     }
 }
 
 void markdown_generator::visit(image& image) {
-    top = create_child(top, CMARK_NODE_IMAGE);
+    top = append_child(top, CMARK_NODE_IMAGE);
     cmark_node_set_title(top, image.title.c_str());
     cmark_node_set_url(top, image.src.c_str());
     stream_generator::visit(image);
@@ -168,7 +174,7 @@ void markdown_generator::visit(image& image) {
 }
 
 void markdown_generator::visit(thematic_break& thematic_break) {
-    create_child(top, CMARK_NODE_THEMATIC_BREAK);
+    append_child(top, CMARK_NODE_THEMATIC_BREAK);
 }
 
 }
